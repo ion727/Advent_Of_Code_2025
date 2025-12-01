04da35af29f9edb8fab997a4e3d7824f1e3cd3b9
ion <151961936+ion727@users.noreply.github.com>
2025-12-01 15:23:53 +0100

    Add files via upload

---
+++ Day-1/pt1.py
@@ -0,0 +1,20 @@
+"""
+author: @ion727
+
+Advent Of Code: Day 1 (part 1)
+"""
+
+password = 0
+with open("data.txt", "r") as fp:
+    datalines = fp.readlines()
+    ptr = 50
+    for data in datalines:
+        sign, *num = data
+        num = int("".join(num).strip("\n"))
+        num *= -1 if sign == "L" else 1
+        ptr += num
+        ptr %= 100
+        if ptr == 0:
+            password += 1
+print(password)
+# Answer: 995
---
+++ Day-1/pt2.py
@@ -0,0 +1,21 @@
+"""
+author: @ion727
+
+Advent Of Code: Day 1 (part 2)
+"""
+
+password = 0
+with open("data.txt", "r") as fp:
+    datalines = fp.readlines()
+    ptr = 50
+    for data in datalines:
+        sign, *num = data
+        num = int("".join(num).strip("\n"))
+        coeff = -1 if sign == "L" else 1
+        for _ in range(num):
+            ptr += coeff
+            ptr %= 100
+            if ptr == 0:
+                password += 1
+print(password)
+# Answer: 5847
