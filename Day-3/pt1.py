256449416ce1b64f3b727eec2498326516eef0b9
ion727 <ionizer727@gmail.com>
2025-12-03 22:40:46 +0100

    Completed Day 3.

---
+++ Day-3/pt1.py
@@ -0,0 +1,23 @@
+"""
+author: @ion727
+
+Advent Of Code: Day 3 (part 1)
+"""
+
+total = 0
+with open("example.txt", "r") as fp:
+    datalines = fp.read().split("\n")
+    for line in datalines:
+        first, second = 0,0
+        end = len(line)-1
+        for index,char in enumerate(line):
+            num = int(char)
+            if num > first and index != end:
+                first = num
+                second = 0
+            elif num > second:
+                second = num
+            print(list(map(int,list(str(a)))))
+        total += (a:=first*10 + second)
+print(total)
+# Solution: 17408
---
+++ Day-3/pt2.py
@@ -0,0 +1,33 @@
+"""
+author: @ion727
+
+Advent Of Code: Day 3 (part 2)
+"""
+VERBOSE = False
+
+total = 0
+with open("data.txt", "r") as fp:
+    datalines = fp.read().split("\n")
+    for line in datalines:
+        n = 12
+        vals = [0]*n
+        length = len(line)
+        if VERBOSE:print(f"check line:{line}")
+        for index, char in enumerate(line): # iterate through items in line
+            if VERBOSE:print(f"\n\ncheck index {index}: {char}")
+            num = int(char)
+            for k, best_val in enumerate(vals): # iterate through stored values
+                if VERBOSE:print(f"compare element {k}: {best_val} to element {index}: {char}")
+                if index - length + n <=  k and num > best_val:
+                    if VERBOSE:print("successful comparison, assigning new value and replacing subsequents")
+                    vals[k] = num
+                    if VERBOSE:print(f"1. state after initial replacement of index {k}: {vals}")
+                    # replace every next value with 0
+                    for i in range(k+1, n):
+                        if VERBOSE:print(f"replacing element {i}")
+                        vals[i] = 0
+                    if VERBOSE:print(f"replacemenent completed: {vals}\n")
+                    break
+        total += int(''.join(list(map(str, vals))))
+print(total)
+# Solution: 172740584266849
